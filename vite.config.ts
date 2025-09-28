320b8c2447ca9276e92bde4caf4d195ee1f2904b
Ankit Pathak <ankit@Ankit.ankit>
2025-09-28 19:45:23 +0530

    edited error

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }: { mode: string }) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  return {
+    server: {
+      host: "::",
+      port: 8080,
+    },
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
